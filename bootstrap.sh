8aa43cf65af2a63ba76a93f807c9e24cd1dab519
user <vabole@gmail.com>
2017-01-30 15:34:40 +0300

    fixed tor repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,24 +1,46 @@
 #!/usr/bin/env bash
 
 #initial one-liner already got all updates
-#sudo apt update && sudo apt upgrade
+
+#tools to use apt over https
+sudo apt-get install apt-transport-https \
+                       ca-certificates
+
+#install add-apt
+sudo apt-get install software-properties-common
 
 #install basics cli
 sudo apt install -y vim htop zsh xclip
-#baisics gui
+#basics gui
 sudo apt install -y zeal default-jre terminator diodon chromium-browser  
 
 #networking tools
 sudo apt install -y whois nmap zmap traceroute wget httpie
 
+sudo apt install -y vagrant
+
 #battery saving tools including thinkpad specific
 sudo apt install -y tlp tlp-rdw tp-smapi-dkms acpi-call-dkms 
+
+#docker dependencies
+sudo apt-get install curl \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
 
 #remove flash in case it was installed
 sudo apt purge -y flashplugin-installer
 
 #remove amazon integrations
 sudo apt purge -y unity-webapps-common
+
+#add docker's GPG key
+curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+
+#add docker's repo 
+sudo add-apt-repository \
+       "deb https://apt.dockerproject.org/repo/ \
+       ubuntu-$(lsb_release -cs) \
+       main"
 
 # adding nodejs repo
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
@@ -27,15 +49,17 @@ curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
-#adding tor repo
-sudo deb http://deb.torproject.org/torproject.org xenial main
-sudo deb-src http://deb.torproject.org/torproject.org xenial main
+#adding tor repo with sources
+sudo add-apt-repository -s 'deb http://deb.torproject.org/torproject.org xenial main' 
 
 #checking key for tor repo
 gpg --keyserver keys.gnupg.net --recv A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | sudo apt-key add -
 
-sudo apt -y update && sudo apt-get install -y nodejs build-essential yarn
+sudo apt -y update 
+sudo apt-get install -y nodejs build-essential 
+sudo apt install yarn
+sudo apt install docker-engine
 sudo apt install -y tor deb.torproject.org-keyring 
 sudo apt install -y torbrowser-launcher
 
